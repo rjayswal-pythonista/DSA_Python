a94060d9b16264ac066af107d3e6e7ee359c080f
rjayswal-pythonista <roshanjayswal15@cisco.com>
2021-08-07 15:58:21 +0530

    updated_change

--- Q4_QueueStack.py
+++ Q4_QueueStack.py
@@ -1,44 +1,70 @@
-#   Created by Roshan Jayswal on 04/06/2020.
-#   Copyright © 2021. All rights reserved.
+# Queqe using Two Stack
 
-# Implement a queue using two stacks.
+class Stack:
+    def __init__(self):
+        self.list = []
+        
+    def __len__(self):
+        if not self.list:
+            return 0
+        return len(self.list)
+    
+    def __str__(self):
+        if not self.list:
+            return None
+        return self.list
+    
+    def push(self, data):
+        return self.list.append(data)
+    
+    def pop(self):
+        if not self.list:
+            return None
+        return self.list.pop()
+    
+    
+class QueqeViaStack:
+    def __init__(self):
+        self.inStack = Stack()
+        self.outStack = Stack()
+    
+    def __str__(self):
+        if not self.inStack.list:
+            return None
+        else:
+            string = ''
+            for i in (self.inStack.list):
+                string += str(i) + ' '
+            return string
+    
+    def enqueqe(self, data):
+        return self.inStack.push(data)
+            
+    def dequeqe(self):
+        if not self.inStack.list:
+            return "Queqe is Empty"
+        while len(self.inStack):
+            self.outStack.push(self.inStack.pop())
+        data = self.outStack.pop()
+        while len(self.outStack):
+            self.inStack.push(self.outStack.pop())
+        
+        return data    
 
-class Stack():
-  def __init__(self):
-    self.list = []
-  
-  def __len__(self):
-    return len(self.list)
-  
-  def push(self, item):
-    self.list.append(item)
-  
-  def pop(self):
-    if len(self.list) == 0:
-      return None
-    return self.list.pop()
+myqueqe = QueqeViaStack()
+for i in range(1,11):
+    myqueqe.enqueqe(i)
+print(myqueqe)
 
-class QueueviaStack():
-  def __init__(self):
-    self.inStack = Stack()
-    self.outStack = Stack()
-  
-  def enqueue(self, item):
-    self.inStack.push(item)
-  
-  def dequeue(self):
-    while len(self.inStack):
-      self.outStack.push(self.inStack.pop())
-    result = self.outStack.pop()
-    while len(self.outStack):
-      self.inStack.push(self.outStack.pop())
-    return result
-  
+print(myqueqe.dequeqe())
+print(myqueqe)
 
-customQueue = QueueviaStack()
-customQueue.enqueue(1)
-customQueue.enqueue(2)
-customQueue.enqueue(3)
-print(customQueue.dequeue())
-customQueue.enqueue(4)
-print(customQueue.dequeue())
+
+"""
+[output]
+
+1 2 3 4 5 6 7 8 9 10
+1
+2 3 4 5 6 7 8 9 10
+
+"""
